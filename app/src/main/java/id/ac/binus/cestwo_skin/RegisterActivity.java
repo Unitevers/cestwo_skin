aa6081c23916c0cdc36475c504a02311ab49268d
william <jeremiah.wsebastian@gmail.com>
2023-12-11 00:04:14 +0700

    register page included, but still ugly in UI, needs improvement

--- app/src/main/java/id/ac/binus/cestwo_skin/RegisterActivity.java
+++ app/src/main/java/id/ac/binus/cestwo_skin/RegisterActivity.java
@@ -1,4 +1,35 @@
 package id.ac.binus.cestwo_skin;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText editName;
+    EditText editUsername;
+    EditText editAge;
+    EditText editEmail;
+    EditText editPhone;
+    Button button_back;
+    Button registerButton;
+    CheckBox checkTnC;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        editName =findViewById(R.id.editName);
+        editUsername = findViewById(R.id.editUsername);
+        editAge = findViewById(R.id.editAge);
+        editEmail = findViewById(R.id.editEmail);
+        editPhone = findViewById(R.id.editPhone);
+
+        button_back = findViewById(R.id.button_back);
+        registerButton = findViewById(R.id.registerButton);
+        checkTnC = findViewById(R.id.checkTnC);
+
+    }
 }
