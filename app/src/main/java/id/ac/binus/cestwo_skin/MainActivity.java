79ce0cc28c509850d4763969f246411c8e2ba2f2
Andre Theofilus Permana <91383519+Unitevers@users.noreply.github.com>
2023-12-11 23:51:40 +0700

    main activity layout interface

--- app/src/main/java/id/ac/binus/cestwo_skin/MainActivity.java
+++ app/src/main/java/id/ac/binus/cestwo_skin/MainActivity.java
@@ -5,20 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editEmail;
+    EditText editPassword;
+    Button loginButton;
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editEmail = findViewById(R.id.logEmail);
+        editPassword = findViewById(R.id.logPassword);
+        loginButton = findViewById(R.id.loginButton);
 
-        new Handler().postDelayed(new Runnable() {
+        db = new DatabaseHelper(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
-                finish();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
             }
-        }, 4000);
+        });
     }
 }
